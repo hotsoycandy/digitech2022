ad9291a36aecb10c3f3ebe1063e1d8ed4f2615b5
hotsoycandy <wkdwnsghd617@gmail.com>
2023-08-04 21:22:43 +0900

    5주차

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,10 @@
-import express from 'express'
-import multer from 'multer'
 import { connectDB } from './db'
-import {
-  path as signupPath,
-  method as signupMethod,
-  handler as signupHandler
-} from './routes/signup'
-
-async function startServer (): Promise<void> {
-  const app = express()
-  const upload = multer({ dest: 'static/' })
-
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-  app.use(upload.fields([{ name: 'ooo' }]))
-  app.use('/static', express.static('static'))
-
-  app[signupMethod](signupPath, signupHandler)
-
-  const port = 3000
-  await new Promise((resolve) => {
-    app.listen(port, (): void => {
-      resolve(null)
-    })
-  })
-  console.log('Server is ready!')
-}
+import { startServer } from './server'
 
 async function main (): Promise<void> {
   await connectDB()
   await startServer()
-  console.log('Service is ready!')
+  console.log('Applicaiton is ready!')
 }
 
 main().catch(console.error)
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,10 +1,28 @@
 /** @file 회원가입 API 정보 파일 */
 
 import { Request, Response } from 'express'
+import User from '../Models/User'
 
 const path = '/signup'
-const method = 'put'
+const method = 'post'
 const handler = (req: Request, res: Response): Response => {
+  console.log(1)
+  User.create({
+    name: '권기범',
+    age: 26,
+    gender: '아파치 헬리콥터',
+    id: 'kill_kimgjungun',
+    password: 'qwer1234',
+    phoneNumber: '010-1234-5678',
+    email: 'kill_kimgjungun@sdh.hs.kr',
+    friendsNumber: 5,
+    inflowPath: '북한',
+    houseAddress: '평양시'
+  }).then(() => {
+    console.log('create success')
+  }).catch(err => {
+    console.error(err)
+  })
   return res.send('Hello, World!')
 }
 
